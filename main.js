34277adc3cd2d3ad9490b0abe73451ed37ea8d1f
suhanashakirshaikh <83486875+suhanashakirshaikh@users.noreply.github.com>
2022-12-18 09:30:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+song =  "";
+
+
+function preload(){
+     song = loadSound("music.mp3");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses)
+}
+
+function gotPoses(results)
+{
+    if(results.length>0)
+    {
+        console.log(results);
+    }
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialzed');
+}
+
+function draw(){
+    Image(video, 0, 0, 600, 500);
+}
+function play()
+{
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+
+}
